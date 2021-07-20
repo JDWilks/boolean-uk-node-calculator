3582efadd8b03997f5d942d5768ca172db6449a6
Jonathon Wilks <jonathondwilks@icloud.com>
2021-07-20 18:13:34 +0100

    calculator saved - on challenge one

--- script.js
+++ script.js
@@ -2,13 +2,35 @@ const { clear } = require("node:console");
 
 const input = process.argv;
 
-console.log("this ithe the process argv", process.argv);
+console.log("this is the process argv", process.argv);
 
-const firstNumber = Number(input[2]);
-const operator = input[3];
-const secondNumber = Number(input[4]);
+// const divide = "/"
+// const multiply = "*"
+// const add = "+"
+// const substract = "-"
+
+const newInput = input.slice(2);
+
+console.log("this is new array", newInput);
+
+let divIndex = newInput.indexOf("/");
+let divArray = newInput.splice(divIndex - 1, divIndex + 2);
+
+// let multipyIndex = numbersArrayCalc.indexOf("*");
+// let multipyArray = numbersArrayCalc.splice(divIndex -1, divIndex +2)
+
+// let addIndex = numbersArrayCalc.indexOf("+");
+// let addArray = numbersArrayCalc.splice(divIndex -1, divIndex +2)
+
+// let subtractIndex = numbersArrayCalc.indexOf("/");
+// let subtractArray = numbersArrayCalc.splice(divIndex -1, divIndex +2)
+
+const firstNumber = Number(divArray[0]);
+const operator = divArray[1];
+const secondNumber = Number(divArray[2]);
 
 function calculator(firstNumber, operator, secondNumber) {
+  let result = 0;
   if (operator === "add" || operator === "+") {
     result = firstNumber + secondNumber;
   } else if (operator === "minus" || operator === "-") {
@@ -30,6 +52,8 @@ function calculator(firstNumber, operator, secondNumber) {
 }
 
 console.log("your result is:", calculator(firstNumber, operator, secondNumber));
+
+// below is code that worked as a test
 
 // function multiply(firstNumber, secondNumber) {
 //     return firstNumber * secondNumber;
